26d6acdda374647e65a6ae145e6813e65aaf6d71
sathishcs20 <sathishj.cs20@bitsathy.ac.in>
2022-11-16 18:59:25 +0530

    testing router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express=require('express')
 const app =express()
 
 app.use(express.static("../public"))
+app.set("view engine","hbs")
+app.set("views","../views")
+
+app.get("/",(req,res)=>
+{
+    res.render("index")
+})
 app.listen(3000,()=>
 {
     console.log('server started')
