ff9799b5d6c6ef9f35303f19109264a97b375cfa
sathishcs20 <sathishj.cs20@bitsathy.ac.in>
2022-11-20 10:21:17 +0530

    adding css

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,55 @@
-console.log("hi")
+const formid=document.getElementById('form')
+const btnloc=document.getElementById("btnloc")
+const message=document.getElementById('message')
+const msgbtn=document.getElementById('msgbtn')
+const msg=document.getElementById("msg")
+const messageTemplate=document.getElementById("message-template").innerHTML
+const locationTemplate=document.getElementById("location-template").innerHTML
+
+const socket=io()
+
+const {username,room} = Qs.parse(location.search,{ignoreQueryPrefix : true})
+
+socket.on('message',(m)=>
+{
+    
+    const html=Mustache.render(messageTemplate,{message:m.message,createdAt:moment(m.createdAt).format("h:mm a")})
+    msg.insertAdjacentHTML("beforeend",html)
+})
+socket.on('locationMessage',(m)=>
+{
+     
+    const html=Mustache.render(locationTemplate,{message:m.message,createdAt:moment(m.createdAt).format("h:mm a")})
+    msg.insertAdjacentHTML("beforeend",html)
+})
+
+
+formid.addEventListener('submit',(e)=>
+{
+    e.preventDefault()
+    socket.emit("Sendmessage",message.value,()=>
+    {
+        console.log("message delivered")
+        message.value=""
+        message.focus()
+    })
+})
+btnloc.addEventListener('click',(e)=> 
+{
+    e.preventDefault()
+    if(!navigator.geolocation)
+    {
+        return alert("not supported")
+    }
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+        socket.emit("sendLocation",position.coords.latitude,position.coords.longitude,()=>
+        {
+            console.log("location sent")
+            
+        })
+    })
+})
+
+socket.emit("join",{username,room})
+
