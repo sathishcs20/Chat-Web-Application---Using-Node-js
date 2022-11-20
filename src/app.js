a986c3b2dc699180433b93b7d92a3ba0bb2460dd
sathishcs20 <sathishj.cs20@bitsathy.ac.in>
2022-11-20 10:19:23 +0530

    socket.io room

--- src/app.js
+++ src/app.js
@@ -11,7 +11,16 @@ app.use(express.static("../public"))
 
 io.on("connection",(socket)=>
 {
-    socket.broadcast.emit("message",generateMessage("New user joined")) //broadcast.emit will emit the message to all the connections except the one joined
+    // socket.broadcast.emit("message",generateMessage("New user joined")) //broadcast.emit will emit the message to all the connections except the one joined
+
+    socket.on("join",({username,room})=>
+    {
+        socket.join(room)
+        socket.broadcast.to(room).emit("message",generateMessage(username+" joined")) 
+
+        //io.emit           socket.broadcast.emit
+        //io.to().smit      socket.broadcast.to().smit  these two will emit messages to a particular room
+    })
     socket.on("Sendmessage",(message,callback)=>
     {
         io.emit("message",generateMessage(message)) //io.emit will emit message to all the connections
